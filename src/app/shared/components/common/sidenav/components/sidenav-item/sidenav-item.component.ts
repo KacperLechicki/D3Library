4a5c9339c9375b82bfff258434918f85dbe48a04
kacperlechicki <klechicki@poczta.fm>
2024-01-15 19:39:07 +0100

    feature: sidenav items

--- src/app/shared/components/common/sidenav/components/sidenav-item/sidenav-item.component.ts
+++ src/app/shared/components/common/sidenav/components/sidenav-item/sidenav-item.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'sh-sidenav-item',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidenav-item.component.html',
   styleUrl: './sidenav-item.component.scss',
 })
-export class SidenavItemComponent {}
+export class SidenavItemComponent {
+  @Input() path = '';
+}
--- src/app/shared/components/common/sidenav/sidenav.component.ts
+++ src/app/shared/components/common/sidenav/sidenav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AsyncPipe, NgClass } from '@angular/common';
 import { SidenavStateService } from '../../../store/sidenav/sidenav-state.service';
+import { SidenavItemComponent } from './components/sidenav-item/sidenav-item.component';
 
 @Component({
   selector: 'sh-sidenav',
   standalone: true,
-  imports: [AsyncPipe, NgClass],
+  imports: [AsyncPipe, NgClass, SidenavItemComponent],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
