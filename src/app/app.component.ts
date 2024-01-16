2056fc4b68466e47a475782a29ce9239314c435c
kacperlechicki <klechicki@poczta.fm>
2024-01-16 09:33:43 +0100

    feature: reactive width of content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,37 @@ import { TopbarComponent } from './shared/components/common/topbar/topbar.compon
 import { SidenavComponent } from './shared/components/common/sidenav/sidenav.component';
 import { BootstrapStateService } from './shared/services/bootstrap-state.service';
 import { RouterOutlet } from '@angular/router';
+import { Observable, debounceTime, fromEvent, map, startWith } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TopbarComponent, SidenavComponent, RouterOutlet],
+  imports: [TopbarComponent, SidenavComponent, RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements AfterViewInit {
-  readonly title = 'd3-library';
+  public readonly title = 'd3-library';
+  protected sidenavWidth = 0;
+
+  private readonly desktopBreakpoint = 1200;
 
   constructor(private bootstrapStateService: BootstrapStateService) {}
 
   ngAfterViewInit(): void {
     this.bootstrapStateService.bootstrap();
+  }
+
+  protected isDesktop(): Observable<boolean> {
+    return fromEvent(window, 'resize').pipe(
+      debounceTime(500),
+      startWith(this.getScreenSize()),
+      map(() => this.getScreenSize())
+    );
+  }
+
+  private getScreenSize(): boolean {
+    return window.innerWidth > this.desktopBreakpoint;
   }
 }
--- src/app/shared/components/common/sidenav/sidenav.component.ts
+++ src/app/shared/components/common/sidenav/sidenav.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+  effect,
+} from '@angular/core';
 import { AsyncPipe, NgClass } from '@angular/common';
 import { SidenavStateService } from '../../../store/sidenav/sidenav-state.service';
 import { SidenavItemComponent } from './components/sidenav-item/sidenav-item.component';
@@ -11,8 +18,22 @@ import { SidenavItemComponent } from './components/sidenav-item/sidenav-item.com
   styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
+  @ViewChild('sidenav', { read: ElementRef }) sidenav!: ElementRef<HTMLElement>;
+  @Output() sidenavWidth = new EventEmitter<number>();
+
   protected sidenavOpen = this.sidenavStateService.select('opened');
   protected readonly logoPath = 'assets/logo/logo.png';
 
   constructor(private sidenavStateService: SidenavStateService) {}
+
+  private _sidenavOpenChange = effect((): void => {
+    if (this.sidenavOpen()) {
+      setTimeout((): void => {
+        const width = this.sidenav.nativeElement.offsetWidth;
+        this.sidenavWidth.emit(width);
+      }, 800);
+    } else {
+      this.sidenavWidth.emit(0);
+    }
+  });
 }
