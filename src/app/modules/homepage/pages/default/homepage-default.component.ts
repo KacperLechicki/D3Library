2432be1da4358946ec19b472f350ea18740757ab
kacperlechicki <klechicki@poczta.fm>
2024-01-16 21:51:59 +0100

    fix: small fixes

--- src/app/modules/homepage/pages/default/homepage-default.component.ts
+++ src/app/modules/homepage/pages/default/homepage-default.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/components/controls/button/button.component';
+import { GlowingDotBgComponent } from '../../../../shared/components/common/glowing-dots-bg/glowing-dots-bg.component';
 
 @Component({
   selector: 'ui-homepage-default',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, GlowingDotBgComponent],
   templateUrl: './homepage-default.component.html',
   styleUrl: './homepage-default.component.scss',
 })
